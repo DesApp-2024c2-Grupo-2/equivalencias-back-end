016384a39182c1541d2e6eb0332405930b0d3118
stkener <stkener@gmail.com>
2024-11-10 22:00:58 -0300

    modifique todos los migrations add relacion mensaje equiv

--- db/migrations/20230823035253-add-relacion-mensaje-equivalencia.js
+++ db/migrations/20230823035253-add-relacion-mensaje-equivalencia.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn(
       'Mensajes',
@@ -20,4 +20,30 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Mensajes', 'id_equivalencia');
   }
+};*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla para verificar si la columna "id_equivalencia" ya existe
+    const tableDescription = await queryInterface.describeTable('Mensajes');
+
+    // Verificar si la columna "id_equivalencia" ya existe
+    if (!tableDescription.id_equivalencia) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Mensajes', 'id_equivalencia', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Equivalencia', // Referencia a la tabla 'Equivalencia'
+          key: 'id', // Columna de referencia en 'Equivalencia'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna "id_equivalencia" si existe
+    return queryInterface.removeColumn('Mensajes', 'id_equivalencia');
+  },
 };
