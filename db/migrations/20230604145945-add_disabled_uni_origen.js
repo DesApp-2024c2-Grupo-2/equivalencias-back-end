e153ad2fdbad57ca9d311c8537723f6a2351223c
stkener <stkener@gmail.com>
2024-11-10 21:05:17 -0300

    modifique todos los migrations add disabled uni origen

--- db/migrations/20230604145945-add_disabled_uni_origen.js
+++ db/migrations/20230604145945-add_disabled_uni_origen.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Universidad_origen', 'disabled', {
       type: Sequelize.BOOLEAN,
@@ -12,5 +12,30 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Universidad_origen', 'disabled');
+  },
+};*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla para verificar si la columna "disabled" ya existe
+    const tableDescription = await queryInterface.describeTable(
+      'Universidad_origen'
+    );
+
+    // Verificar si la columna "disabled" ya existe
+    if (!tableDescription.disabled) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Universidad_origen', 'disabled', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false, // Valor predeterminado para la nueva columna
+        after: 'sigla', // Especificar la columna después de la cual se debe colocar
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna "disabled" si existe
+    return queryInterface.removeColumn('Universidad_origen', 'disabled');
   },
 };
