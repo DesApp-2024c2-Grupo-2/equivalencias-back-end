303dd45d38a093d861f6913a983eaa562a290d0e
stkener <stkener@gmail.com>
2024-11-10 23:03:39 -0300

    modifique todos los migrations carreraorigen a materia aprobada

--- db/migrations/20231012210504-agregar-carreraOrigen-a-materia_aprobada.js
+++ db/migrations/20231012210504-agregar-carreraOrigen-a-materia_aprobada.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Materia_aprobada', 'carreraOrigen', {
       type: Sequelize.STRING,
@@ -8,6 +8,28 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Materia_aprobada', 'carreraOrigen');
+  },
+};*/
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla 'Materia_aprobada' para verificar si la columna 'carreraOrigen' ya existe
+    const tableDescription = await queryInterface.describeTable(
+      'Materia_aprobada'
+    );
+
+    // Verificar si la columna 'carreraOrigen' ya existe
+    if (!tableDescription.carreraOrigen) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Materia_aprobada', 'carreraOrigen', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna 'carreraOrigen' si existe
     return queryInterface.removeColumn('Materia_aprobada', 'carreraOrigen');
   },
 };
