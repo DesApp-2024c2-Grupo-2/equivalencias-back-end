9c52d393093ffd3b6d5a767c597535f7a458cce5
stkener <stkener@gmail.com>
2024-11-10 22:51:13 -0300

    modifique todos los migrations add relacion equivalencia carrera

--- db/migrations/20230823035306-add-relacion-equivalencia-carrera.js
+++ db/migrations/20230823035306-add-relacion-equivalencia-carrera.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn(
       'Equivalencia',
@@ -20,4 +20,30 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Equivalencia', 'CarreraId');
   }
+};*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla para verificar si la columna "CarreraId" ya existe
+    const tableDescription = await queryInterface.describeTable('Equivalencia');
+
+    // Verificar si la columna "CarreraId" ya existe
+    if (!tableDescription.CarreraId) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Equivalencia', 'CarreraId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Carrera', // Referencia a la tabla 'Carrera'
+          key: 'id', // Columna de referencia en 'Carrera'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna "CarreraId" si existe
+    return queryInterface.removeColumn('Equivalencia', 'CarreraId');
+  },
 };
