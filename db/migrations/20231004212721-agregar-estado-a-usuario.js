3549e27839756d00b8882813039f1ae77a680b3d
stkener <stkener@gmail.com>
2024-11-10 22:57:17 -0300

    modifique todos los migrations agregar estado a usuario

--- db/migrations/20231004212721-agregar-estado-a-usuario.js
+++ db/migrations/20231004212721-agregar-estado-a-usuario.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Usuarios', 'estado', {
       type: Sequelize.STRING,
@@ -9,6 +9,26 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Usuarios', 'estado');
+  },
+};*/
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla 'Usuarios' para verificar si la columna 'estado' ya existe
+    const tableDescription = await queryInterface.describeTable('Usuarios');
+
+    // Verificar si la columna 'estado' ya existe
+    if (!tableDescription.estado) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Usuarios', 'estado', {
+        type: Sequelize.STRING,
+        allowNull: true, // Opcional, dependiendo de tus requisitos
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna 'estado' si existe
     return queryInterface.removeColumn('Usuarios', 'estado');
   },
 };
