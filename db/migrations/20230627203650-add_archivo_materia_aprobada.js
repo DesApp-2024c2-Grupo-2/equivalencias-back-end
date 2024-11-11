e05810877975931ec31f58f8e21aba1f1818144a
stkener <stkener@gmail.com>
2024-11-10 21:12:48 -0300

    modifique todos los migrations add archivo materia aprobada

--- db/migrations/20230627203650-add_archivo_materia_aprobada.js
+++ db/migrations/20230627203650-add_archivo_materia_aprobada.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Materia_aprobada', 'archivo', {
       type: Sequelize.STRING,
@@ -12,5 +12,29 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Materia_aprobada', 'archivo');
+  },
+};*/
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla para verificar si la columna "archivo" ya existe
+    const tableDescription = await queryInterface.describeTable(
+      'Materia_aprobada'
+    );
+
+    // Verificar si la columna "archivo" ya existe
+    if (!tableDescription.archivo) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Materia_aprobada', 'archivo', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null, // Valor predeterminado para la nueva columna
+        after: 'certificado', // Especificar la columna después de la cual se debe colocar
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna "archivo" si existe
+    return queryInterface.removeColumn('Materia_aprobada', 'archivo');
   },
 };
