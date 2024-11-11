6461f963f9d43b9819e88b94a8b0077d15a4fcd1
stkener <stkener@gmail.com>
2024-11-11 00:30:53 -0300

    modifique sedders usuarios carreras

--- db/seeders/20230908042928-Usuarios_carreras~.js
+++ db/seeders/20230908042928-Usuarios_carreras~.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     const [carrera] = await queryInterface.sequelize.query(
       `SELECT * FROM "Usuarios_carreras" WHERE CarreraId = 1;`
@@ -8,6 +8,57 @@ module.exports = {
 
     // Si no existe la carrera, entonces insertarla
     if (!carrera) {
+      return queryInterface.bulkInsert('Usuarios_carreras', [
+        {
+          UsuarioId: 2,
+          CarreraId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 2,
+          CarreraId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 2,
+          CarreraId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 6,
+          CarreraId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 6,
+          CarreraId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+    } else {
+      console.log('La carrera ya existe. No se insertó.');
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Usuarios_carreras', null, {});
+  },
+};*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const [carrera] = await queryInterface.sequelize.query(
+      `SELECT * FROM "Usuarios_carreras" WHERE "CarreraId" = 1;` // Usar "CarreraId" entre comillas si la columna tiene mayúsculas
+    );
+
+    // Si no existe la carrera, entonces insertarla
+    if (!carrera || carrera.length === 0) {
+      // Asegurarse de que el resultado esté vacío antes de insertar
       return queryInterface.bulkInsert('Usuarios_carreras', [
         {
           UsuarioId: 2,
