a78cea2d2a53165af28853bf093a15fc81b4bd79
stkener <stkener@gmail.com>
2024-11-10 21:56:25 -0300

    modifique todos los migrations agregar carrera en mat solic

--- db/migrations/20230821011930-agregar-carrera-en-materia-solicitada.js
+++ db/migrations/20230821011930-agregar-carrera-en-materia-solicitada.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = {
+/*module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Materia_solicitada', 'carrera', {
       type: Sequelize.STRING,
@@ -15,5 +15,27 @@ module.exports = {
       // nombre de la columna
       'carrera'
     );
+  },
+};*/
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Describir la tabla para verificar si la columna "carrera" ya existe
+    const tableDescription = await queryInterface.describeTable(
+      'Materia_solicitada'
+    );
+
+    // Verificar si la columna "carrera" ya existe
+    if (!tableDescription.carrera) {
+      // Agregar la columna solo si no existe
+      await queryInterface.addColumn('Materia_solicitada', 'carrera', {
+        type: Sequelize.STRING,
+        after: 'EquivalenciumId', // Especificar la columna después de la cual se debe colocar
+      });
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Eliminar la columna "carrera" si existe
+    return queryInterface.removeColumn('Materia_solicitada', 'carrera');
   },
 };
